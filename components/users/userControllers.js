095abd7cf16c9d60e2a48301419e1e0a96e2741d
quan-illogical <quan.nguyen@terralogic.com>
2020-09-03 16:56:51 +0700

    fixed user register error handling

--- components/users/userControllers.js
+++ components/users/userControllers.js
@@ -3,16 +3,28 @@ const User = require("./userModels");
 exports.createUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const user = new User({
+    const dupUser = await User.findOne({ username });
+    const dupEmail = await User.findOne({ email });
+    if (dupEmail && dupUser) {
+      throw new Error("This username and email has already been registered")
+    }
+    if (dupUser) {
+      throw new Error("This username has already been registered");
+    }
+    if (dupEmail) {
+      throw new Error("This email has already been registered")
+    }
+      const user = new User({
         username,
         email,
-        password
-    })
+        password,
+      });
+
+      await user.save();
+      const token = await user.generateToken();
+      res.status(200).json({ status: "OK", data: { user, token } });
     
-    await user.save()
-    const token = await user.generateToken();
-    res.status(200).json({status: "OK", data: {user, token}})
   } catch (err) {
-      res.status(400).json({status: "FAILED", error: err.message})
+    res.status(400).json({ status: "FAILED", error: err.message });
   }
 };
